189f4f01d6a4d5646faa86ac84bff945a9623670
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-01-09 16:23:18 -0700

    Added a new image

--- index.php
+++ index.php
@@ -8,7 +8,10 @@
 	</head>
 	<body>
 		<p><h1>This is the story about Sarah Ruth</h1></p>
-		<img id="kitties" src="images/sacco-chilla.jpg">
-		<p><h3><em>Sarah Ruth lives with two cute furries, Sacco and Chilla.</em><br>Sarah Ruth also lives with her partner and is a caretaker to her uncle.</h3></p>
+		<img id="sacco-chilla" src="images/sacco-chilla.jpg">
+		<img id="sarahruth-ballon" scr="images/sarahruth-ballon.jpg">
+		<p><h3><em>Sarah Ruth lives with two cute furries, Sacco and Chilla.</em><br>
+			Sarah Ruth also lives with her partner and is a primary caretaker to her uncle.<br>
+			Sarah Ruth is originally from Chicago, she is an artist</h3></p>
 	</body>
 </html>
