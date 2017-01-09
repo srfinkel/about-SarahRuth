459722c58fb08d24e06fb823daed26838523ecf7
Sarah Ruth Finkel <srfinkel@gmail.com>
2017-01-09 15:47:30 -0700

    Uploaded .css file

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 		<title>About Sarah Ruth</title>
 	</head>
 	<body>
-		<p>This is the story about Sarah Ruth</p>
+		<p><h1>This is the story about Sarah Ruth</h1></p>
+		<img src="images/sacco-chilla.jpg">
+		<p><h3><em>Sarah Ruth lives with two cute furries, Sacco and Chilla</em></h3></p>
 	</body>
 </html>
