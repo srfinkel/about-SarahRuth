157c3aa3cfb0550efb07176fd763bd18e9fa17a8
Solomon Leyba <solomon.leyba@gmail.com>
2017-01-10 09:41:41 -0700

    Updated html and css settings

--- index.php
+++ index.php
@@ -10,8 +10,8 @@
 		<h1>This is the <b>TRUE</b> story about <em>Sarah Ruth</em></h1>
 		<img id="sacco-chilla" src="images/sacco-chilla.jpg">
 		<img id="sarahruth-ballon" src="images/sarahruth-ballon.jpg">
-		<h3 id="aboutMe">(~: Sarah Ruth lives with two <em>cute</em> furries, Sacco and Chilla :~)<br>
+		<h3 id="aboutMe">(~: Sarah Ruth lives with two(maybe three) <em>cute</em> furries, Sacco and Chilla :~)<br>
 			Sarah Ruth also lives with her partner and is a primary caretaker to her uncle.<br>
-			Sarah Ruth is originally from Chicago, she is an artist</h3>
+			Sarah Ruth is originally from Chicago(or is she?), she is an artist</h3>
 	</body>
 </html>
