d2d5e0416589ef7d6ce8c205b0a94fba83c73968
Solomon Leyba <solomon.leyba@gmail.com>
2017-01-10 09:25:18 -0700

    Updated html and css settings

--- index.php
+++ index.php
@@ -7,11 +7,11 @@
 		</style>
 	</head>
 	<body>
-		<p><h1>This is the story about Sarah Ruth</h1></p>
+		<h1>This is the story about <em>Sarah Ruth</em></h1>
 		<img id="sacco-chilla" src="images/sacco-chilla.jpg">
-		<img id="sarahruth-ballon" scr="images/sarahruth-ballon.jpg">
-		<p><h3><em>Sarah Ruth lives with two cute furries, Sacco and Chilla.</em><br>
+		<img id="sarahruth-ballon" src="images/sarahruth-ballon.jpg">
+		<h3 id="aboutMe">(~: Sarah Ruth lives with two <em>cute</em> furries, Sacco and Chilla :~)<br>
 			Sarah Ruth also lives with her partner and is a primary caretaker to her uncle.<br>
-			Sarah Ruth is originally from Chicago, she is an artist</h3></p>
+			Sarah Ruth is originally from Chicago, she is an artist</h3>
 	</body>
 </html>
